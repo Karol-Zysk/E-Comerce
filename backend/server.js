0f6393a9cdf0b85fc0d9136d8757897c9705bd00
Karol Zyśk <trener.karolzysk@gmail.com>
2022-03-30 11:10:22 +0200

    PORT change

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const productRoutes = require("./routes/productRoutes");
 
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
